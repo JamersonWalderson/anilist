b9d5b0c062ab6c2041d01ccd65c135a011618143
JamersonWalderson <jamersonwalderson@gmail.com>
2023-06-17 17:05:27 -0300

    feat: input e botão, feio, mas funciona

--- lib/main.dart
+++ lib/main.dart
@@ -30,9 +30,6 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-
-class 
-
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
@@ -49,17 +46,21 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
+      body: Container(
+        alignment: Alignment.center,
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            TextFormField(
+                decoration: const InputDecoration(
+                    hintText: "Exemplo: naruto",
+                    border: UnderlineInputBorder(
+                        borderSide:
+                            BorderSide(color: Colors.red, width: 2.0)))),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('GetValue'),
+            )
           ],
         ),
       ),
